eca38465c5b63a604c24eeed175834f9a2a56a4f
vakkineni <vakkineni@backcountry.com>
2021-05-18 14:29:05 -0600

    change message

--- test_app.py
+++ test_app.py
@@ -18,7 +18,7 @@ from app import hello
 class TestHelloApp(unittest.TestCase):
 
   def test_hello(self):
-    self.assertEqual(hello(), "Hello World!\n")
+    self.assertEqual(hello(), "Hello cloud build!\n")
 
 if __name__ == '__main__':
   unittest.main()
